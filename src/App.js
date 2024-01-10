affab6441f1b97d1eba3fb771310cd2c5eba704c
NazarHural <147272629+NazarHural@users.noreply.github.com>
2024-01-10 20:38:38 +0200

    portfolio

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import './App.css';
+
+import NavBar from './Components/NavBar/NavBar.js';
+
+export default function App() {
+  return (
+    <>
+      <NavBar />
+    </>
+  );
+}
